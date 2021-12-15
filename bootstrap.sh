dc44e33439b1ef755232f475227b95dc174f775d
Luc Perkins <luc.perkins@datadoghq.com>
2021-12-15 13:53:52 -0800

    Add HM initialization to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-TMP=`mktemp -d`
-
 # First, move this repo to $HOME/.config/nixpkgs
 # git clone https://github.com/lucperkins/nix-home-manager-config ~/.config/nixpkgs
 
@@ -12,3 +10,6 @@ sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-v
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
+
+# Initiate Home Manager
+home-manager switch && source "$HOME/.zshrc"

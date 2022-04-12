bfeaf0488a25cd73dd010339d0f25317fbfe4120
Luc Perkins <lucperkins@gmail.com>
2022-04-12 14:10:30 -0700

    Fix error in init script

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-lsource /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 # Run the env.sh script (if it exists). That script is meant to contain secrets, tokens, and
 # other things you don't want to put in your Nix config
